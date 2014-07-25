f6b1935a49a8c32520e37c637e6096cfc0a95ea8
Fedor Indutny <fedor@indutny.com>
2014-07-26 00:46:26 +0400

    toools: grow trees, array primitives

--- tools.js
+++ tools.js
@@ -1,6 +1,43 @@
+//
+// ARRAY
+//
+
+function array_alloc(n) {
+  if (n === 0)
+    throw new Error('Array is too small');
+  if (n === 1)
+    return 0;
+
+  var i = 0;
+  var res = 0;
+  while (i < n) {
+    res = [ 0, res ];
+    i = i + 1;
+  }
+  return res;
+}
+
+function array_get(a, i) {
+  while (i > 0) {
+    if (typeof a === 'object')
+      a = a[1];
+    else
+      a = 0;
+    i = i - 1;
+  }
+  if (typeof a === 'object')
+    return a[0];
+  else
+    return a;
+}
+
+//
+// TREE
+//
+
 function tree_alloc(n) {
   if (n === 0)
-    throw new Error('Array too small');
+    throw new Error('Tree is too small');
   if (n === 1)
     return 0;
 
@@ -26,6 +63,16 @@ function tree_set(a, i, v) {
 
   var half = i / 2;
   var r = i - half * 2;
+
+  // Grow tree automatically
+  if (typeof a === 'number') {
+    if (r === 0)
+      return [ tree_set(0, half, v), a ];
+    else
+      return [ a, tree_set(0, half, v) ];
+  }
+
+  // Just a normal insertion
   if (r === 0)
     return [ tree_set(a[0], half, v), a[1] ];
   else
