09c385ae549d30996d4183122edf5edc5e00b92e
Fedor Indutny <fedor@indutny.com>
2014-07-27 14:33:59 +0400

    compiler: fix !

--- ghc/lib/preparser.js
+++ ghc/lib/preparser.js
@@ -1,5 +1,4 @@
 var assert = require('assert');
-var ssa = require('ssa-ir');
 
 function Preparser(source) {
   this.source = source;
@@ -9,7 +8,7 @@ function Preparser(source) {
   this.labels = {};
   this.missingLabels = {};
 
-  var root = new Block(this, '$root');
+  var root = new Block(this, '_root');
   this.current = root;
   this.blocks.push(root);
 
@@ -45,20 +44,25 @@ Preparser.prototype.preparse = function preparse() {
     // Add instruction
     this.visitInstr(line);
   }
+
+  return this.blocks.map(function(b) { return b.toJSON(); });
 };
 
 Preparser.prototype.visitLabel = function visitLabel(name) {
   var b = new Block(this, name);
 
   // Blocks follow each other
+  if (this.current.instructions.length === 0)
+    this.current.add('nop', null, [], null);
   this.current.go(b);
   this.current = b;
-
   this.blocks.push(b);
   this.labels[name] = b;
+
   if (this.missingLabels[name]) {
     var q = this.missingLabels[name];
     delete this.missingLabels[name];
+
     for (var i = 0; i < q.length; i++)
       q[i].from.go(b);
   }
@@ -77,6 +81,12 @@ Preparser.prototype.visitInstr = function visitInstr(instr) {
     args = [];
   } else {
     assert(args.every(function(arg) { return arg.length !== 0 }), 'empty arg');
+  }
+
+  // Just a literal
+  if (type == (type | 0)) {
+    args = [ type ];
+    type = 'literal';
   }
 
   var label = null;
@@ -117,36 +127,145 @@ Preparser.prototype.visitInstr = function visitInstr(instr) {
     });
   }
 
-  // Split the block
   if (!this.current.ended)
-    this.current = this.current.sub();
+    this.current = this.current.after();
 };
 
 function Block(preparser, id) {
   this.preparser = preparser;
 
   this.id = id;
+  this.predecessors = [];
   this.successors = [];
   this.instructions = [];
   this.ended = false;
 
   this.parent = null;
-  this.subs = [];
+  this.befores = [];
+  this.afters = [];
+  this.splits = [];
 }
+
+Block.prototype.toJSON = function toJSON() {
+  return {
+    id: this.id,
+    instructions: this.instructions.map(function(instr) {
+      return instr.toJSON();
+    }),
+    successors: this.successors.map(function(s) {
+      return s.id;
+    })
+  };
+};
 
 Block.prototype.go = function go(block) {
   assert(block);
-  if (!this.ended)
-    this.successors.push(block);
+  if (this.ended)
+    return this;
+
+  var from = block;
+  var to = this;
+
+  // Auto-split block to simplify graph
+  if (from.predecessors.length === 2)
+    from = from.before();
+
+  from.predecessors.push(to);
+  to.successors.push(from);
+  assert(from.predecessors.length <= 2);
+  assert(to.successors.length <= 2);
+
+  // Create a junction
+  if (to.predecessors.length === 2 && to.successors.length === 2)
+    to = to.split();
 };
 
-Block.prototype.sub = function sub() {
+Block.prototype.before = function before() {
+  var p;
   if (this.parent)
-    return this.parent.sub();
+    p = this.parent;
+  else
+    p = this;
 
-  var b = new Block(this.preparser, this.id);
-  b.parent = this;
-  b.id += '$' + this.subs.push(b);
+  var b = new Block(this.preparser, p.id);
+  b.parent = p;
+  b.id = '_' + b.id + '_a' + p.befores.unshift(b);
+
+  b.add('nop', null, [], null);
+
+  this.preparser.blocks.push(b);
+  b.go(this);
+
+  // Re-link blocks
+  b.predecessors = this.predecessors.slice(1);
+  this.predecessors = this.predecessors.slice(0, 1);
+  b.predecessors.forEach(function(pred) {
+    pred.successors = pred.successors.map(function(succ) {
+      if (succ === this)
+        return b;
+      else
+        return succ;
+    }, this);
+  }, this);
+
+  return b;
+};
+
+Block.prototype.after = function after() {
+  var p;
+  if (this.parent)
+    p = this.parent;
+  else
+    p = this;
+
+  var b = new Block(this.preparser, p.id);
+  b.parent = p;
+  b.id = '_' + b.id + '_a' + p.afters.push(b);
+
+  // Re-link blocks
+  b.successors = this.successors.slice(1);
+  this.successors = this.successors.slice(0, 1);
+  b.successors.forEach(function(succ) {
+    succ.predecessors = succ.predecessors.map(function(pred) {
+      if (pred === this)
+        return b;
+      else
+        return pred;
+    }, this);
+  }, this);
+
+  this.preparser.blocks.push(b);
+  this.go(b);
+
+  return b;
+};
+
+Block.prototype.split = function split() {
+  var p;
+  if (this.parent)
+    p = this.parent;
+  else
+    p = this;
+
+  var b = new Block(this.preparser, p.id);
+  b.parent = p;
+  b.id = '_' + b.id + '_s' + p.splits.push(b);
+
+  b.add('nop', null, [], null);
+
+  b.predecessors = this.predecessors;
+  this.predecessors = [];
+  b.predecessors.forEach(function(pred) {
+    pred.successors = pred.successors.map(function(succ) {
+      if (succ === this)
+        return b;
+      else
+        return succ;
+    }, this);
+  }, this);
+
+  this.preparser.blocks.push(b);
+  b.go(this);
 
   return b;
 };
@@ -162,13 +281,21 @@ Block.prototype.add = function add(type, out, inputs, label) {
 
 function Instruction(block, type, output, inputs, label) {
   this.block = block;
-  this.id = block.preparser.instructionId++;
+  this.id = 'i' + block.preparser.instructionId++;
 
   this.type = type;
-  this.output = output;
+  this.output = output || { type: 'instruction', id: this.id };
   this.inputs = inputs;
   this.label = label;
 
-  if (!this.output)
-    this.output = { type: 'instruction', id: this.id };
+  this.assign = this.output ? this.output.type === 'variable' : false;
 }
+
+Instruction.prototype.toJSON = function toJSON() {
+  return {
+    id: this.assign ? this.output.id : this.id,
+    type: this.type,
+    inputs: this.inputs,
+    assign: this.assign
+  };
+};
